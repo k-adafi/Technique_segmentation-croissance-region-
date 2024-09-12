7030441ecf5a7625b7535af3b8180819a468ed9b
k-adafi <kadafiben196@gmail.com>
2024-09-12 20:33:09 +0300

    Finalisation du segmentation

--- main.py
+++ main.py
@@ -16,16 +16,16 @@ hauteurImage = 440
 # Créer la fenêtre principale
 window = tk.Tk()
 window.title("Segmentation Image - CROISSANCE D'UNE REGION")
-window.geometry("1500x660")
+window.geometry("1380x660")
 window.minsize(600, 350)
 
 # Changer la couleur de fond
 window.configure(bg='lightblue')
 
-
 # Variables globales
 imageOriginale = None
 imageBinaireGris = None
+seed_point = None
 
 
 # Fonction pour ouvrir l'image
@@ -33,12 +33,14 @@ def open_image(label):
     global imageOriginale
     file_path = askopenfilename()
     if file_path:
-        imageOriginale = cv2.imread(file_path)  # Lire l'image avec OpenCV
-        image_pil = Image.open(file_path)
-        image_pil = image_pil.resize((largeurImage, hauteurImage), Image.LANCZOS)  # Redimensionner l'image
-        photo = ImageTk.PhotoImage(image_pil)
-        label.config(image=photo)
-        label.image = photo
+        img = Image.open(file_path)
+        img = img.resize((largeurImage, hauteurImage), Image.LANCZOS)
+        imgpil = ImageTk.PhotoImage(img)
+        imageOriginale = cv2.imread(file_path)  # Lire l'image en niveaux de gris
+        imageOriginale = cv2.resize(imageOriginale, (largeurImage, hauteurImage))
+        image_pil = Image.fromarray(imageOriginale)
+        label.config(image=imgpil)
+        label.image = imgpil
 
 
 # Fonction pour quitter l'application
@@ -47,71 +49,48 @@ def quit_app():
         window.destroy()
 
 
-# Fonction pour convertir l'image en niveaux de gris et l'afficher
-def imageBinaire():
-    global imageOriginale, imageBinaireGris
-    if imageOriginale is None:
-        messagebox.showerror("Erreur", "Aucune image n'a été chargé!")
-        return
-    else:
-        # Conversion en niveaux de gris
-        imageBinaireGris = cv2.cvtColor(imageOriginale, cv2.COLOR_BGR2GRAY)
-
-        # Affichage dans image_label2
-        image_pil = Image.fromarray(imageBinaireGris)
-        image_pil = image_pil.resize((largeurImage, hauteurImage), Image.LANCZOS)
-        photo = ImageTk.PhotoImage(image_pil)
-        image_label2.config(image=photo)
-        image_label2.image = photo
-
-
-# Fonction pour l'étiquetage des composants connexes
-def etiqueter_image():
-    global imageBinaireGris
-    if imageBinaireGris is None:
-        messagebox.showerror("Erreur", "Aucune image binaire n'a été convertie!")
-        return
-
-    # Seuillage binaire pour s'assurer que l'image est bien binaire
-    _, binary_image = cv2.threshold(imageBinaireGris, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
-    # Inverser les couleurs pour avoir le fond noir et les composants blancs
-    binary_image = cv2.bitwise_not(binary_image)
-
-    # Etiquetage des composants connexes
-    num_labels, labels_im = cv2.connectedComponents(binary_image)
-
-    # Normalisation des étiquettes pour une meilleure visualisation
-    labels_im = (labels_im * 255 / (num_labels - 1)).astype(np.uint8)
-
-    # Affichage dans image_label3
-    image_pil = Image.fromarray(labels_im)
-    image_pil = image_pil.resize((largeurImage, hauteurImage), Image.LANCZOS)
-    photo = ImageTk.PhotoImage(image_pil)
-    image_label3.config(image=photo)
-    image_label3.image = photo
-
-
-# Fonction pour vider les cases
 def viderCases():
-    global image_label1, image_label2, image_label3
+    global image_label1, image_label2
 
     # Vider les images dans les labels
-    image_label1.config(image='')
-    image_label2.config(image='')
+    image_label1.config(image="", text="")
+    image_label2.config(image="", text="")
 
     # Réinitialiser les références d'image pour éviter les fuites de mémoire
     image_label1.image = None
     image_label2.image = None
 
-    #Vider le textefield
+    # Vider les textefield
+    text_X.delete(0, tk.END)
+    text_Y.delete(0, tk.END)
 
 
-# Fonction pour enregistrer les images
+def SegmenterRegion(image, seed_point, threshold=10):
+    # Convertir l'image en niveaux de gris si elle est en couleur
+    if len(image.shape) == 3:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    rows, cols = image.shape[:2]
+    output = np.zeros((rows, cols), np.uint8)
+    to_explore = [seed_point]
+    start_value = image[seed_point[0], seed_point[1]]  # Extraire la valeur du pixel de départ
+
+    while to_explore:
+        x, y = to_explore.pop(0)
+        output[x, y] = 255
+
+        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < rows and 0 <= ny < cols:
+                if output[nx, ny] == 0 and abs(int(image[nx, ny]) - int(start_value)) < threshold:
+                    to_explore.append((nx, ny))
+                    output[nx, ny] = 127
+
+    return output
+
+
 def enregistrer_images():
-    global image_label1, image_label2, image_label3
-
-    # Demander à l'utilisateur de choisir un dossier pour sauvegarder les images
+    global image_label1, image_label2
     save_directory = tk.filedialog.askdirectory()
     if not save_directory:
         return
@@ -119,47 +98,82 @@ def enregistrer_images():
     if image_label1.image is None:
         messagebox.showerror("Erreur", "Aucun image n'est ajouté.")
     else:
-        # Sauvegarder image_label1
         if image_label1.image:
             image_pil1 = ImageTk.getimage(image_label1.image)
             image_path1 = os.path.join(save_directory, "image1.png")
             image_pil1.save(image_path1)
             print(f"Image 1 enregistrée à {image_path1}")
 
-        # Sauvegarder image_label2
         if image_label2.image:
             image_pil2 = ImageTk.getimage(image_label2.image)
             image_path2 = os.path.join(save_directory, "image2.png")
             image_pil2.save(image_path2)
             print(f"Image 2 enregistrée à {image_path2}")
 
-        # Sauvegarder image_label3
-        if image_label3.image:
-            image_pil3 = ImageTk.getimage(image_label3.image)
-            image_path3 = os.path.join(save_directory, "image3.png")
-            image_pil3.save(image_path3)
-            print(f"Image 3 enregistrée à {image_path3}")
-
         # Confirmation
         messagebox.showinfo("Enregistrement", "Les images ont été enregistrées avec succès.")
         viderCases()
+
+
+def on_mouse_click(event):
+    global seed_point
+    seed_point = (event.y, event.x)
+    print(f"Point de départ sélectionné : {seed_point}")
+    text_X.delete(0, tk.END)
+    text_X.insert(0, str(seed_point[1]))
+    text_Y.delete(0, tk.END)
+    text_Y.insert(0, str(seed_point[0]))
+
+
+def on_mouse_move(event):
+    image_label1.config(text=f"Coordonnées : ({event.x}, {event.y})")
+
+
+def segment_image():
+    global imageOriginale, seed_point
+    if imageOriginale is not None:
+        try:
+            x = int(text_X.get())
+            y = int(text_Y.get())
+            seed_point = (y, x)
+            print(f"Point de départ défini à partir du formulaire : {seed_point}")
+        except ValueError:
+            if seed_point is None:
+                messagebox.showerror("Erreur",
+                                     "Veuillez entrer des coordonnées valides ou sélectionner un point de départ avec la souris.")
+                return
+
+        # Vérifier si les coordonnées sont dans les limites de l'image
+        if seed_point[0] >= imageOriginale.shape[0] or seed_point[1] >= imageOriginale.shape[1]:
+            messagebox.showerror("Erreur", "Les coordonnées du point de départ sont en dehors des limites de l'image.")
+            return
+
+        segmented_image = SegmenterRegion(imageOriginale, seed_point)
+        image_pil = Image.fromarray(segmented_image)
+        image_pil = image_pil.resize((largeurImage, hauteurImage), Image.LANCZOS)
+        photo = ImageTk.PhotoImage(image_pil)
+        image_label2.config(image=photo)
+        image_label2.image = photo
+    else:
+        messagebox.showerror("Erreur", "Veuillez d'abord ajouter une image.")
 
 
 # Boucle pour les frames
 for ligne in range(1):
     for colonne in range(2):
         image_frame1 = tk.Frame(window, bg='white', bd=2, relief='sunken', width=largeur, height=hauteur)
-        image_frame1.grid(row=2, column=0, padx=0, pady=10)
+        image_frame1.grid(row=2, column=0, padx=0, pady=6)
         image_frame1.grid_propagate(False)  # Empêcher le cadre de se redimensionner
 
         image_frame2 = tk.Frame(window, bg='white', bd=2, relief='sunken', width=largeur, height=hauteur)
-        image_frame2.grid(row=2, column=1, padx=0, pady=10)
+        image_frame2.grid(row=2, column=1, padx=0, pady=6)
         image_frame2.grid_propagate(False)
 
-
 # Créer des labels pour afficher les images dans les cadres de chaque frames
-image_label1 = tk.Label(image_frame1, bg='white', bd=2, relief='solid')
+image_label1 = tk.Label(image_frame1, bg='white', bd=2, relief='solid', cursor="cross")
 image_label1.pack(expand=True, fill='both')
+image_label1.bind("<Button-1>", on_mouse_click)
+image_label1.bind("<Motion>", on_mouse_move)
 
 image_label2 = tk.Label(image_frame2, bg='white', bd=2, relief='solid')
 image_label2.pack(expand=True, fill='both')
@@ -185,14 +199,13 @@ label_Y.grid(row=1, column=0, padx=5, pady=0)
 text_Y = tk.Entry(frame_commande, width=40)
 text_Y.grid(row=1, column=1, padx=5, pady=0)
 
-
 # Créer des boutons
 button1 = tk.Button(window, text="Ajouter image", font=("Berlin sans FB", 14), bg='#5187ec', fg="white",
                     command=lambda: open_image(image_label1), width=30, height=1)
 button1.grid(row=4, column=0, padx=20, pady=3)
 
 button2 = tk.Button(window, text="Segmenter l'image", font=("Berlin sans FB", 14), bg='#5187ec', fg="white",
-                    command=imageBinaire, width=30, height=1)
+                    command=segment_image, width=30, height=1)
 button2.grid(row=4, column=1, padx=20, pady=3)
 
 button4 = tk.Button(window, text="Enregistrer image", font=("Berlin sans FB", 14), bg='#5187ec', fg="white",
@@ -211,100 +224,10 @@ window.config(menu=menu)
 file_menu = tk.Menu(menu, tearoff=0, bg="lightblue", fg="black", font=("Berlin sans FB", 14))
 menu.add_cascade(label="Fichier", menu=file_menu, font=("Berlin sans FB", 14))
 file_menu.add_command(label="Ajouter image", command=lambda: open_image(image_label1))
-file_menu.add_command(label="Segmenter image", command=imageBinaire)
+file_menu.add_command(label="Segmenter image", command=segment_image)
+file_menu.add_command(label="Enregistrer image", command=enregistrer_images)
 file_menu.add_command(label="Vider les cases", command=viderCases)
 file_menu.add_command(label="Quitter", command=quit_app)
 
 # Lancer la boucle principale de l'interface graphique
 window.mainloop()
-
-
-"""import cv2
-import numpy as np
-import tkinter as tk
-from tkinter import filedialog
-from tkinter import simpledialog
-from PIL import Image, ImageTk
-
-
-def region_growing(img, seed, threshold):
-    height, width = img.shape
-    segmented_img = np.zeros((height, width), np.uint8)
-    segmented_img[seed] = 255
-    region_mean = img[seed]
-    region_size = 1
-    pixel_list = [seed]
-
-    while pixel_list:
-        current_pixel = pixel_list.pop(0)
-        for neighbor in get_neighbors(current_pixel, height, width):
-            if segmented_img[neighbor] == 0 and abs(int(img[neighbor]) - int(region_mean)) < threshold:
-                segmented_img[neighbor] = 255
-                pixel_list.append(neighbor)
-                region_mean = (region_mean * region_size + img[neighbor]) / (region_size + 1)
-                region_size += 1
-
-    return segmented_img
-
-
-def get_neighbors(pixel, height, width):
-    x, y = pixel
-    neighbors = []
-    for dx in [-1, 0, 1]:
-        for dy in [-1, 0, 1]:
-            if dx == 0 and dy == 0:
-                continue
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < height and 0 <= ny < width:
-                neighbors.append((nx, ny))
-    return neighbors
-
-
-def load_image():
-    file_path = filedialog.askopenfilename()
-    if file_path:
-        img = cv2.imread(file_path, 0)
-        display_image(img)
-        return img
-    return None
-
-
-def display_image(img):
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-    img_pil = Image.fromarray(img_rgb)
-    img_tk = ImageTk.PhotoImage(img_pil)
-    panel.config(image=img_tk)
-    panel.image = img_tk
-
-
-def segment_image():
-    if img is not None:
-        seed_x = simpledialog.askinteger("Input", "Enter seed x-coordinate:")
-        seed_y = simpledialog.askinteger("Input", "Enter seed y-coordinate:")
-        threshold = simpledialog.askinteger("Input", "Enter threshold:")
-        seed = (seed_y, seed_x)
-        segmented_img = region_growing(img, seed, threshold)
-        display_image(segmented_img)
-
-
-# Création de la fenêtre principale
-root = tk.Tk()
-root.title("Segmentation image")
-
-
-# Boutons pour charger et segmenter l'image
-btn_load = tk.Button(root, text="Charger Image", command=load_image)
-btn_load.pack()
-
-btn_segment = tk.Button(root, text="Segmenter Image", command=segment_image)
-btn_segment.pack()
-
-# Panneau pour afficher l'image
-panel = tk.Label(root)
-panel.pack()
-
-# Variable pour stocker l'image chargée
-img = None
-
-# Lancement de la boucle principale de Tkinter
-root.mainloop()"""
